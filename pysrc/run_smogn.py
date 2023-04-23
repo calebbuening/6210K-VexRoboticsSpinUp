c23e793a8f607cb65151a08804e1317a5e802704
Caleb Buening <caleb.j.buening@gmail.com>
2023-04-23 15:02:57 -0400

    Smogn Fix

--- pysrc/run_smogn.py
+++ pysrc/run_smogn.py
@@ -2,9 +2,9 @@ import pandas
 import smogn
 import seaborn
 
-df = pandas.read_csv("C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\data\\data.csv")
+df = pandas.read_csv("data.csv")
 
-df_smogn = smogn.smoter(data=df, y='speed')
+df_smogn = smogn.smoter(data=df, y='Speed')
 
 seaborn.kdeplot(df['SalePrice'], label = "Original")
 seaborn.kdeplot(df_smogn['SalePrice'], label = "Modified")
