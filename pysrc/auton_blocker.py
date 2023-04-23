fe488c77d77f77193d89a4934bfb1c130b5f840e
Caleb Buening <caleb.j.buening@gmail.com>
2023-04-23 15:00:39 -0400

    Pysrc remote updates

--- pysrc/auton_blocker.py
+++ pysrc/auton_blocker.py
@@ -9,13 +9,13 @@ from keras2cpp import export_model
 import smogn
 
 # edit this boolean if producing and not just testing
-SAVE = False
+SAVE = True
 
 # create the model as a function
 def create_model(use_smogn=False):
 	# Customize adam learning rate. Changing will require retrain.
 	# Read csv into pandas dataframe
-	dataframe = pd.read_csv("C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\data\\data.csv")
+	dataframe = pd.read_csv("data.csv")
 	if use_smogn: dataframe = smogn.smoter(data=dataframe, y="Speed")
 	# split into input (X) and output (Y) variables (values will be floats as they are floats in the c++ program)
 	dataset = dataframe.values
@@ -29,30 +29,30 @@ def create_model(use_smogn=False):
 	# This is not an algorithm because I didn't want to write two identical normalization algorithms for python and c++
 	model.add(BatchNormalization(input_shape=(5,)))
 	# "input" layer
-	model.add(Dense(5, activation='relu'))
+	model.add(Dense(5, activation='tanh'))
 	# Can customize but will require a retrain
-	model.add(Dense(9, activation='relu'))
-	model.add(Dense(12, activation='relu'))
-	model.add(Dense(8, activation='relu'))
-	model.add(Dense(6, activation='relu'))
-	model.add(Dense(3, activation='relu'))
+	model.add(Dense(9, activation='tanh'))
+	model.add(Dense(12, activation='tanh'))
+	model.add(Dense(8, activation='tanh'))
+	model.add(Dense(6, activation='tanh'))
+	model.add(Dense(3, activation='tanh'))
 	# single neuron output for action value
 	# relu activation as first test
 	model.add(Dense(1, kernel_initializer='normal', activation='linear'))
 	# compile showing accuracy and using mse and adam as they performed the best
 	model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=.001), metrics=['accuracy'])
 	# checkpoint
-	filepath="C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\src\\weights-best.hdf5"
+	filepath="weights-best.hdf5"
 	checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
 	callbacks_list = [checkpoint]
 	# can modify batch size and epochs but will require a retrain
 	model.summary()
-	model.fit(X, Y, batch_size=50, epochs=5000, verbose=1, callbacks=callbacks_list)
+	model.fit(X, Y, batch_size=10, epochs=100, verbose=1, callbacks=callbacks_list)
 	return model
 
 def load_model(use_smogn=False):
 	model = Sequential()
-	dataframe = pd.read_csv("C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\data\\data.csv")
+	dataframe = pd.read_csv("data.csv")
 	if use_smogn: dataframe = smogn.smoter(data=dataframe, y="Speed")
 	# split into input (X) and output (Y) variables (values will be floats as they are floats in the c++ program)
 	dataset = dataframe.values
@@ -70,10 +70,10 @@ def load_model(use_smogn=False):
 	# single neuron output for action value
 	# relu activation as first test
 	model.add(Dense(1, kernel_initializer='normal', activation='linear'))
-	model = model.load_weights("C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\src\\weights-best.hdf5")
+	model = model.load_weights("weights-best.hdf5")
 	model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=.001), metrics=['accuracy'])
 	# checkpoint
-	filepath="C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\src\\weights-best.hdf5"
+	filepath="weights-best.hdf5"
 	checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
 	callbacks_list = [checkpoint]
 	# can modify batch size and epochs but will require a retrain
@@ -86,8 +86,8 @@ new = input("Train new model? [Y/n] ")
 if new == "Y" or "y":
 	model = create_model()
 	if SAVE:
-		export_model(model, "C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\src\\auton_blocker.model")
+		export_model(model, "auton_blocker.model")
 elif new == "N" or "n":
 	model = load_model()
 	if SAVE:
-		export_model(model, "C:\\Users\\mcurn\\OneDrive\\Documents\\GitHub\\6210K-VexRoboticsSpinUp\\src\\auton_blocker.model")
+		export_model(model, "auton_blocker.model")
